5f0a579faa098d1a161d439a17cb0d220a6d56d0
Ezekiel Clark <clarkzeke@gmail.com>
2020-01-11 13:31:24 -0700

    Add key defi

---
+++ lib/key.rb
@@ -0,0 +1,6 @@
+class Key
+
+  def initialize
+  end
+
+end
--- test/key_test.rb
+++ test/key_test.rb
@@ -6,6 +6,8 @@ class KeyTest < Minitest::Test
 
   def test_it_exists
     key = Key.new
+    
+    assert_instance_of Key, key
   end
 
 end
