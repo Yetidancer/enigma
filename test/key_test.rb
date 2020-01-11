c55f19392b7aaa1dc5a2f6cd100771696ad12614
Ezekiel Clark <clarkzeke@gmail.com>
2020-01-11 14:14:50 -0700

    Add test for random number

--- test/key_test.rb
+++ test/key_test.rb
@@ -1,6 +1,8 @@
 require './lib/key'
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'mocha/minitest'
+
 
 class KeyTest < Minitest::Test
 
@@ -16,6 +18,14 @@ class KeyTest < Minitest::Test
 
   def test_it_generates_alph_upon_initialize
     assert_equal 27, @key.alphabet.length
+  end
+
+  def test_it_can_generate_random_5_digit_number
+    assert_equal 5, @key.random_number.to_s.split('').length
+
+    @key.stubs(:random_number).returns(12345)
+
+    assert_equal 12345, @key.random_number
   end
 
 end
