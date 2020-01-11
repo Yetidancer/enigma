2cc4f5d0de01f0fc3d7a27f1e464c281a293ca76
Ezekiel Clark <clarkzeke@gmail.com>
2020-01-11 14:02:25 -0700

    Add test for alph upon initialize and @key setup

--- test/key_test.rb
+++ test/key_test.rb
@@ -4,10 +4,18 @@ require 'minitest/pride'
 
 class KeyTest < Minitest::Test
 
+  def setup
+    @key = Key.new
+  end
+
   def test_it_exists
     key = Key.new
-    
+
     assert_instance_of Key, key
+  end
+
+  def test_it_generates_alph_upon_initialize
+    assert_equals 27, @key.alphabet
   end
 
 end
