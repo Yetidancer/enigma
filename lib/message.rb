5104686fcae95d472f6efae9773007b98f0bc6df
Ezekiel Clark <clarkzeke@gmail.com>
2020-01-13 22:45:20 -0700

    Clean up message class

--- lib/message.rb
+++ lib/message.rb
@@ -33,36 +33,12 @@ class Message
     text_to_array_specific(-3,@d_array)
   end
 
-  # def shift_text_array_specific(key,text_array,shift_number,shifted_array)
-  #   text_array.each do |letter|
-  #     new_index = ((key.alphabet.index(letter) + shift_number) % 27)
-  #     shifted_array << key.alphabet[new_index]
-  #   end
-  # end
-
-  # def reverse_shift_text_array_specific(key,text_array,shift_number,shifted_array)
-  #   text_array.each do |letter|
-  #     new_index = ((key.alphabet.index(letter) - (shift_number % 27)) % 27)
-  #     if new_index < 0
-  #       new_index += 27
-  #     end
-  #     shifted_array << key.alphabet[new_index]
-  #   end
-  # end
-
   def shift_text_array(key)
     shift_text_array_specific(key, @a_array, key.a_shift, @a_shifted)
     shift_text_array_specific(key, @b_array, key.b_shift, @b_shifted)
     shift_text_array_specific(key, @c_array, key.c_shift, @c_shifted)
     shift_text_array_specific(key, @d_array, key.d_shift, @d_shifted)
   end
-
-  # def reverse_shift_text_array(key)
-  #   reverse_shift_text_array_specific(key, @a_array, key.a_shift, @a_shifted)
-  #   reverse_shift_text_array_specific(key, @b_array, key.b_shift, @b_shifted)
-  #   reverse_shift_text_array_specific(key, @c_array, key.c_shift, @c_shifted)
-  #   reverse_shift_text_array_specific(key, @d_array, key.d_shift, @d_shifted)
-  # end
 
   def reform_shifted_text(key)
     @shifted_text = make_shifted_array(key).join
