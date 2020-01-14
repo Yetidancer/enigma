27e9372603b48d33e2a93c9568d637c9ae65fde0
Ezekiel Clark <clarkzeke@gmail.com>
2020-01-14 11:51:29 -0700

    Add method for decryption to enigma class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -13,4 +13,14 @@ class Enigma
     encryption = {encryption: message1.shifted_text, key: key, date: date}
   end
 
+  def decrypt(message, key, date)
+    message1 = DecryptedMessage.new(message)
+    key1 = Key.new(key, date)
+
+    key1.letter_shift
+    message1.translate_message(key1)
+
+    decryption = {decryption: message1.shifted_text, key: key, date: date}
+  end
+
 end
