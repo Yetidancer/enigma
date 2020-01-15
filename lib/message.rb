784c7007246d3d43ee4f3d36444e66d235bda082
Ezekiel Clark <clarkzeke@gmail.com>
2020-01-14 18:55:02 -0700

    Edit methods to make tests work

--- lib/message.rb
+++ lib/message.rb
@@ -2,7 +2,7 @@ class Message
 
   attr_reader :text, :a_array, :b_array, :c_array, :d_array, :a_shifted, :b_shifted, :c_shifted, :d_shifted, :shifted_text
   def initialize(text)
-    @text = text
+    @text = text.downcase
     @a_array = []
     @b_array = []
     @c_array = []
@@ -31,6 +31,17 @@ class Message
     text_to_array_specific(-1,@b_array)
     text_to_array_specific(-2,@c_array)
     text_to_array_specific(-3,@d_array)
+  end
+
+  def shift_text_array_specific(key,text_array,shift_number,shifted_array)
+    text_array.each do |letter|
+      if key.alphabet.include?(letter)
+        new_index = ((key.alphabet.index(letter) + shift_number) % 27)
+        shifted_array << key.alphabet[new_index]
+      else
+        shifted_array << letter
+      end
+    end
   end
 
   def shift_text_array(key)
